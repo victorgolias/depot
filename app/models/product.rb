9ca8a790b3d340167513843bad573ae11fd75afa
Viktor Golias <golias.viktor@gmail.com>
2011-10-19 14:25:30 -0700

    added line items scaffold

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
 
   # validation stuff
   validates :title, :description, :image_url, :presence => true
@@ -9,4 +10,17 @@ class Product < ActiveRecord::Base
       :with => %r{\.(gif|jpg|png)$}i,
       :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
